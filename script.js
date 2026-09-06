d387b77e640ed6b09faa97fef106106868b475ac
Murilo Teodoro Almeida <murilosupergamer.2020@gmail.com>
2026-09-06 11:46:56 -0300

    animações funcionais de troca de questão, hover e active

--- script.js
+++ script.js
@@ -1,73 +1,17 @@
-// DATASET
 let current = 0;
 let points = 0;
 const quizConteiner = document.querySelector(".quiz");
+
 quizConteiner.addEventListener("animationend", () => {
-  quizConteiner.classList.remove("fadeup")
-  quizConteiner.classList.add("glowup");
-})
-const questions = [
-    {
-        question: "Do you think it's a good idea for hospitals to have medical robots?",
-        a1: "Yes",
-        a2: "No",
-        a3: "Maybe"
-    },
-    {
-        question: "Would you like to be treated by a medical robot or a human doctor?",
-        a1: "Medical robot",
-        a2: "Human",
-        a3: "Both"
-    },
-    {
-        question: "Do you trust medical robots or people more for surgery?",
-        a1: "Robots",
-        a2: "Human",
-        a3: "Both"
-    },
-    {
-        question: "Do medical robots perform work more efficiently than contemporary doctors?",
-        a1: "Yes",
-        a2: "No",
-        a3: "Maybe"
-    },
-    {
-        question: "Do you think medical robots have the potential to cause harmful errors?",
-        a1: "No",
-        a2: "Yes",
-        a3: "Maybe"
-    },
-    {
-        question: "Would you prefer caring and respectful service or an automated, direct approach at a hospital?",
-        a1: "Both",
-        a2: "Respectful and affectionate",
-        a3: "Direct and automated"
-    },
-    {
-        question: "Which do you think ensures greater stability when delivering a diagnosis: a medical robot or a doctor?",
-        a1: "Both",
-        a2: "Doctor",
-        a3: "Medical robot"
-    },
-    {
-        question: "Who do you think will make more mistakes: a programmed medical robot or a medical specialist?",
-        a1: "Both",
-        a2: "Medical robot",
-        a3: "Medical specialist"
-    },
-    {
-        question: "Do you think medical robots offer more benefits than drawbacks?",
-        a1: "Yes",
-        a2: "No",
-        a3: "Maybe"
-    },
-    {
-        question: "Could doctors be more efficient than the technology medical robots offer?",
-        a1: "No",
-        a2: "Yes",
-        a3: "Maybe"
+    if(quizConteiner.classList.contains("fadeup")){
+        
+        quizConteiner.classList.remove("fadeup")
+        quizConteiner.classList.add("glowup") 
+    }else{
+        quizConteiner.classList.remove("glowup")
     }
-]; 
+
+}) 
 
 function update_question(answer){
   document.querySelector(".quiz").classList.add("fadeup")
