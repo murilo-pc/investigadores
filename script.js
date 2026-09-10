19d1b2df79e6153e9db6e2e4707b6caf48101b62
murilo-pc <murilosupergamer.2020@gmail.com>
2026-09-10 12:53:09 -0300

    refactor: organização com troca de onclick por EventListener

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
+// Constantes
+const quizConteiner = document.querySelector(".quiz");
+const question = document.getElementById("question");
+const a1 = document.getElementById("a1");
+const a2 = document.getElementById("a2");
+const a3 = document.getElementById("a3");
+
+// Event Listeners
+a1.addEventListener("click", () => update_question(3));
+a2.addEventListener("click", () => update_question(1));
+a3.addEventListener("click", () => update_question(2));
+quizConteiner.addEventListener("animationend", toggleAnimation)
+
+// Contadores
 let current = 0;
 let points = 0;
-const quizConteiner = document.querySelector(".quiz");
 
-quizConteiner.addEventListener("animationend", () => {
-    if(quizConteiner.classList.contains("fadeup")){
-        quizConteiner.classList.remove("fadeup")
-        quizConteiner.classList.add("glowup") 
-    }else{
-        quizConteiner.classList.remove("glowup")
-    }
-
-}) 
+//Funções
+function toggleAnimation(){
+  if(quizConteiner.classList.contains("fadeup")){
+          quizConteiner.classList.remove("fadeup")
+          quizConteiner.classList.add("glowup") 
+  }else{
+          quizConteiner.classList.remove("glowup")
+  }
+}
 
 function update_question(answer){ // o argumento answer é a quantidade de pontos adicionada
   console.log("answer: "+answer)
@@ -26,20 +39,18 @@ function update_question(answer){ // o argumento answer é a quantidade de ponto
     }else{
       status = "<strong>Pragmatics</strong>: You prefer something objective and straightforward, choosing either a physical doctor or a medical robot, as long as it's the most practical option."
     }
-    document.getElementById("question").innerHTML = "your pontutation is " + points + ", that means you are:<br>" + status
-    document.getElementById("a1").remove()
-    document.getElementById("a2").remove()
-    document.getElementById("a3").remove()
+    question.innerHTML = "your pontutation is " + points + ", that means you are:<br>" + status
+    a1.remove();
+    a2.remove();
+    a3.remove();
 
   }else{
-    document.getElementById("question").textContent = questions[current].question;
-    document.getElementById("a1").textContent = questions[current].a1;
-    document.getElementById("a2").textContent = questions[current].a2;
-    document.getElementById("a3").textContent = questions[current].a3;
+    question.textContent = questions[current].question;
+    a1.textContent = questions[current].a1;
+    a2.textContent = questions[current].a2;
+    a3.textContent = questions[current].a3;
     current++;
   } 
 }
 
-
 update_question(0);
-
